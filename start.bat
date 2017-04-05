e54f770bf25276d547e7f66b8cda810be18b79e7
itsu020402 <itsu020402@gmail.com>
2017-04-06 00:38:28 +0900

    jupiterが存在しないときの処理を追加

--- start.bat
+++ start.bat
@@ -3,8 +3,27 @@
 rem Jupiter 起動バッチファイル(Windows専用)
 
 rem 作成: Itsu
-rem 最終更新: 2017/4/6 0:02
+rem 最終更新: 2017/4/6 0:36
 
-rem 引数で外部プロセスへの出力に対応させています。
+goto Main
+
+:Main
+set Jupiter=nukkit-1.0-SNAPSHOT.jar
+if not exist %Jupiter% (
+	goto Finish
+)
+cls
+chcp 932 > nul
+
 
 java -Djline.terminal=jline.UnsupportedTerminal -jar nukkit-1.0-SNAPSHOT.jar
+
+goto END
+
+:Finish
+	echo %Jupiter%がありません。
+	echo キーを押して終了してください。
+	pause > nul
+
+:END
+	timeout /t 1 > nul

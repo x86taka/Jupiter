b39302395e2f66ebdc0de9020d6515dcfc3326c2
Megapix96 <official.megapixel@gmail.com>
2017-04-13 20:12:06 +0900

    fix

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -358,16 +358,13 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
     	Server.broadcastPacket(this.getLevel().getPlayers().values(), pk);
     }
 
-	public void unlinkHookFromPlayer(){
-		this.fishingHook.close();
-    	this.fishingHook = null;
-    }
-
 	public void unlinkHookFromPlayer(boolean send){
-	    EntityEventPacket pk = new EntityEventPacket();
-	    pk.eid = this.getFishingHook().getId();
-	    pk.event = EntityEventPacket.FISH_HOOK_TEASE;
-	    Server.broadcastPacket(this.getLevel().getPlayers().values(), pk);
+		if (send){
+		    EntityEventPacket pk = new EntityEventPacket();
+		    pk.eid = this.getFishingHook().getId();
+		    pk.event = EntityEventPacket.FISH_HOOK_TEASE;
+		    Server.broadcastPacket(this.getLevel().getPlayers().values(), pk);
+		}
 		this.fishingHook.close();
     	this.fishingHook = null;
     }
@@ -1641,7 +1638,7 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
 
                 if (this.isFishing()){
                 	if (this.distance(this.getFishingHook()) > 33 | this.getInventory().getItemInHand().getId() != Item.FISHING_ROD)
-                		this.unlinkHookFromPlayer();
+                		this.unlinkHookFromPlayer(true);
                 }
             }
 
@@ -2565,7 +2562,7 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                             this.getServer().getPluginManager().callEvent(new PlayerUseFishingRodEvent(this, this.isFishing() ? PlayerUseFishingRodEvent.ACTION_STOP_FISHING : PlayerUseFishingRodEvent.ACTION_START_FISHING));
                             if (!ev.isCancelled()){
 	                            if (this.isFishing()){
-	                            	this.unlinkHookFromPlayer();
+	                            	this.unlinkHookFromPlayer(true);
 	                            } else {
 		                            EntityFishingHook entity = new EntityFishingHook(this.chunk, nbt, this);
 		                            entity.setMotion(entity.getMotion().multiply(f));

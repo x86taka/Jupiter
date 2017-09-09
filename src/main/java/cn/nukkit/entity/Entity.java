4c09b8bada18ecc3ae21120bd2ebd9de8afbfe99
Megapix96 <official.megapixel@gmail.com>
2017-09-09 23:47:12 +0900

    Fix #53

--- src/main/java/cn/nukkit/entity/Entity.java
+++ src/main/java/cn/nukkit/entity/Entity.java
@@ -1396,7 +1396,8 @@ public abstract class Entity extends Location implements Metadatable {
                 dx *= 1.0F + entityCollisionReduction;
                 dz *= 1.0F + entityCollisionReduction;
                 if (this.riding == null) {
-                    setMotion(new Vector3(-dx, 0.0D, -dy));
+                    motionX -= dx;
+                    motionZ -= dy;
                 }
             }
         }
@@ -1839,9 +1840,9 @@ public abstract class Entity extends Location implements Metadatable {
             }
         }
 
-        this.motionX += motion.x;
-        this.motionY += motion.y;
-        this.motionZ += motion.z;
+        this.motionX = motion.x;
+        this.motionY = motion.y;
+        this.motionZ = motion.z;
 
         if (!this.justCreated) {
             this.updateMovement();

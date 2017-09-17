90598006ebe9192a67b43b81ecb00334d96b64fe
itsu020402 <itsu020402@gmail.com>
2017-09-17 19:38:04 +0900

    Update 2

--- src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
+++ src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
@@ -9,9 +9,9 @@ public interface ProtocolInfo {
     /**
      * Actual Minecraft: PE protocol version
      */
-    byte CURRENT_PROTOCOL = 113;
-    String MINECRAFT_VERSION = "v1.1.5";
-    String MINECRAFT_VERSION_NETWORK = "1.1.5";
+    byte CURRENT_PROTOCOL = (byte) 136;
+    String MINECRAFT_VERSION = "v1.2.0";
+    String MINECRAFT_VERSION_NETWORK = "1.2.0";
 
     byte LOGIN_PACKET = 0x01;
     byte PLAY_STATUS_PACKET = 0x02;

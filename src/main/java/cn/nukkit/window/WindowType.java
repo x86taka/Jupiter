a23be6b7db8ab2255ee55393347ad1ea712f30b3
Megapix96 <official.megapixel@gmail.com>
2017-10-02 22:43:43 +0900

    Fix WindowType.java

--- src/main/java/cn/nukkit/window/WindowType.java
+++ src/main/java/cn/nukkit/window/WindowType.java
@@ -4,6 +4,6 @@ public class WindowType {
 	
 	public static final String TYPE_MODAL= "modal";
 	public static final String TYPE_CUSTOM_FORM= "custom_form";
-	public static final String TYPE_SIMPLE_FORM= "simple_form";
+	public static final String TYPE_SIMPLE_FORM= "form";
 
 }

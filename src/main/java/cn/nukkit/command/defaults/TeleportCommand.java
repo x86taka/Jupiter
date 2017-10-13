e8239d008cacbec19230dc15b04bf236f27c391f
itsu020402 <itsu020402@gmail.com>
2017-10-13 19:20:28 +0900

    /tpコマンドでのチルダ記法に対応

--- src/main/java/cn/nukkit/command/defaults/TeleportCommand.java
+++ src/main/java/cn/nukkit/command/defaults/TeleportCommand.java
@@ -1,5 +1,8 @@
 package cn.nukkit.command.defaults;
 
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 import cn.nukkit.Player;
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
@@ -84,38 +87,122 @@ public class TeleportCommand extends VanillaCommand {
             Command.broadcastCommandMessage(sender, new TranslationContainer("commands.tp.success", new String[]{origin.getName(), target.getName()}));
             return true;
         } else if (((Player) target).getLevel() != null) {
+        	
             int pos;
+            
             if (args.length == 4 || args.length == 6) {
                 pos = 1;
             } else {
                 pos = 0;
             }
+            
             double x;
             double y;
             double z;
             double yaw;
             double pitch;
+        	
+        	int times = 0;
+            
             try {
-                x = Double.parseDouble(args[pos++]);
-                y = Double.parseDouble(args[pos++]);
-                z = Double.parseDouble(args[pos++]);
+            	
+            	times = pos++;
+            	
+            	if(args[times].contains("~")){
+            		x = this.parseArgs("x", args[times], target);
+            	}else{
+                    x = Double.parseDouble(args[times]);
+            	}
+            	
+            	times = pos++;
+            	
+            	if(args[times].contains("~")){
+            		y = this.parseArgs("y", args[times], target);
+            	}else{
+                    y = Double.parseDouble(args[times]);
+            	}
+            	
+            	times = pos++;
+            	
+            	if(args[times].contains("~")){
+            		z = this.parseArgs("z", args[times], target);
+            	}else{
+                    z = Double.parseDouble(args[times]);
+            	}
+            	
                 yaw = ((Player) target).getYaw();
                 pitch = ((Player) target).getPitch();
             } catch (NumberFormatException e1) {
                 sender.sendMessage(new TranslationContainer("commands.generic.usage", this.usageMessage));
                 return true;
             }
+            
             if (y < 0) y = 0;
             if (y > 256) y = 256;
+            
             if (args.length == 6 || (args.length == 5 && pos == 3)) {
-                yaw = Integer.parseInt(args[pos++]);
-                pitch = Integer.parseInt(args[pos++]);
+            	
+            	times = pos++;
+            	
+            	if(args[times].contains("~")){
+            		yaw = this.parseArgs("yaw", args[times], target);
+            	}else{
+                    yaw = Double.parseDouble(args[times]);
+            	}
+            	
+            	times = pos++;
+            	
+            	if(args[times].contains("~")){
+            		pitch = this.parseArgs("pitch", args[times], target);
+            	}else{
+            		pitch = Double.parseDouble(args[times]);
+            	}
+            	
             }
+            
             ((Player) target).teleport(new Location(x, y, z, yaw, pitch, ((Player) target).getLevel()), PlayerTeleportEvent.TeleportCause.COMMAND);
             Command.broadcastCommandMessage(sender, new TranslationContainer("commands.tp.success.coordinates", new String[]{target.getName(), String.valueOf(NukkitMath.round(x, 2)), String.valueOf(NukkitMath.round(y, 2)), String.valueOf(NukkitMath.round(z, 2))}));
+            
             return true;
         }
         sender.sendMessage(new TranslationContainer("commands.generic.usage", this.usageMessage));
         return true;
+    }
+    
+    public double parseArgs(String type, String str, CommandSender sender){
+    	
+    	double result = 0;
+    	
+    	switch(type){
+    		case "x":
+        		str = str.replaceAll("~", String.valueOf(((Player) sender).getX()));
+        		break;
+        		
+    		case "y":
+    			str = str.replaceAll("~", String.valueOf(((Player) sender).getY()));
+        		break;
+        		
+    		case "z":
+    			str = str.replaceAll("~", String.valueOf(((Player) sender).getZ()));
+        		break;
+        		
+    		case "yaw":
+    			str = str.replaceAll("~", String.valueOf(((Player) sender).getYaw()));
+        		break;
+        		
+    		case "pitch":
+    			str = str.replaceAll("~", String.valueOf(((Player) sender).getPitch()));
+        		break;
+    	}
+
+    	try {    	
+        	ScriptEngineManager manager = new ScriptEngineManager();
+			result = (double) manager.getEngineByName("JavaScript").eval(str);
+		} catch (ScriptException e) {
+			e.printStackTrace();
+		}
+    	
+		return result;
+    	
     }
 }

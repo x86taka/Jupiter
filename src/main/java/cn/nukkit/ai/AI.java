7e25342e3c5c82024ec25c41bbfa4a68fa74da57
itsu020402 <itsu020402@gmail.com>
2017-11-08 01:02:00 +0900

    AIの研究を開始

---
+++ src/main/java/cn/nukkit/ai/AI.java
@@ -0,0 +1,23 @@
+package cn.nukkit.ai;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.nukkit.entity.Entity;
+
+public class AI {
+
+    private List<Entity> entities;
+
+    public AI() {
+
+    }
+
+    public void init() {
+        entities = new ArrayList<>();
+
+
+
+    }
+
+}

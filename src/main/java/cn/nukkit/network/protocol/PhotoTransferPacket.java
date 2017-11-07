1f60bc7b7aed088946bd3d10100bf43a5bf759e6
itsu020402 <itsu020402@gmail.com>
2017-11-08 00:44:43 +0900

    ...

--- src/main/java/cn/nukkit/network/protocol/PhotoTransferPacket.java
+++ src/main/java/cn/nukkit/network/protocol/PhotoTransferPacket.java
@@ -5,21 +5,24 @@ package cn.nukkit.network.protocol;
  */
 public class PhotoTransferPacket extends DataPacket {
 
-    public String unknownString1;
-    public String unknownString2;
-    public String unknownString3;
+    public String photoName;
+    public String photoData;
+    public String bookId;
 
     public static final byte NETWORK_ID = ProtocolInfo.PHOTO_TRANSFER_PACKET;
 
     @Override
     public void decode() {
-        this.unknownString1 = this.getString();
-        this.unknownString2 = this.getString();
-        this.unknownString3 = this.getString();
+        this.photoName = this.getString();
+        this.photoData = this.getString();
+        this.bookId = this.getString();
     }
 
     @Override
     public void encode() {
+    	this.putString(this.photoName);
+    	this.putString(this.photoData);
+    	this.putString(this.bookId);
     }
 
     @Override

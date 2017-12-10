8d6b34e3d2956f352833b81105d085b7f9e50b3d
popkechupki <hobbypopkechupki@gmail.com>
2017-12-10 11:10:59 +0900

    Re:特定の時にアイテムが表示されないバグを修正
    48695f5

--- src/main/java/cn/nukkit/inventory/PlayerInventory.java
+++ src/main/java/cn/nukkit/inventory/PlayerInventory.java
@@ -454,19 +454,29 @@ public class PlayerInventory extends BaseInventory {
         pk.slot = index;
         pk.item = this.getItem(index).clone();
 
+        boolean check = true;
         for (Player player : players) {
             if (player.equals(this.getHolder())) {
-                pk.inventoryId = ContainerIds.INVENTORY;
-                player.dataPacket(pk);
-            } else {
-                int id = player.getWindowId(this);
-                if (id == -1) {
-                    this.close(player);
-                    continue;
-                }
-                pk.inventoryId = id;
-                player.dataPacket(pk.clone());
+                check = false;
             }
+            int id = player.getWindowId(this);
+            if (id == -1) {
+                this.close(player);
+                continue;
+            }
+            pk.inventoryId = id;
+            player.dataPacket(pk.clone());
+        }
+
+        if (check) {
+            Player player = (Player) this.getHolder();
+            int id = player.getWindowId(this);
+            if (id == -1) {
+                this.close(player);
+                return;
+            }
+            pk.inventoryId = id;
+            player.dataPacket(pk.clone());
         }
     }
 
